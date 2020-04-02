e7930534a77a8f7db8037145a9870083adb8ef67
Maxime Lafont <maxime.lafont.trevisa@gmail.com>
2020-04-02 03:45:00 +0200

    getter,setter + def __init__ de la classe modl_principal

--- pysrc/mdl/systeme-diff-2populations.py
+++ pysrc/mdl/systeme-diff-2populations.py
@@ -1,1 +1,83 @@
- 
+ ####################### Modèle d'étude, sujet 5 #########################
+ class modl_principal:
+ 	def __init__(K1,K2,r1,r2,alpha,beta,gamma,delta):
+ 		self._K1 = K1
+ 		self._K2 = K2
+ 		self._r1 = r1
+ 		self._r2 = r2
+ 		self._alpha = alpha
+ 		self._beta = beta
+ 		self._gamma = gamma
+ 		self._delta = delta
+
+
+ 	def get_K1(self):
+		return self._K1
+
+	def get_K2(self):
+		return self._K1
+
+	def get_r1(self):
+		return self._r1
+
+	def get_r2(self):
+		return self._r2
+
+	def get_alpha(self):
+		return self._alpha
+
+	def get_beta(self):
+		return self._beta
+
+	def get_gamma(self):
+		return self._gamma
+
+	def get_delta(self):
+		return self._delta
+
+	def set_K1(self,K1):
+		self._K1 = K1
+
+	def set_K2(self,K2):
+		self._K2 = K2
+
+	def set_r1(self,r1):
+		self._r1 = r1
+
+	def set_r2(self,r2):
+		self._r2 = r2
+
+	def set_alpha(self,alpha):
+		self._alpha = alpha
+
+	def set_beta(self,beta):
+		self._beta = beta
+
+	def set_gamma(self,gamma):
+		self._gamma = gamma
+
+	def set_delta(self,delta):
+		self._delta = delta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
